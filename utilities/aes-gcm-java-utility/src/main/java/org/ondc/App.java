b0bea71366603583244b094a988952f24a555c19
Subramanyam R <subramanyam.r@searce.com>
2023-05-29 11:21:50 +0530

    Replace System.out with logger in App.java

--- utilities/aes-gcm-java-utility/src/main/java/org/ondc/App.java
+++ utilities/aes-gcm-java-utility/src/main/java/org/ondc/App.java
@@ -1,6 +1,8 @@
 package org.ondc;
 
 import java.security.Security;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.ondc.KeyUtil.DHKeyPair;
@@ -9,6 +11,7 @@ import org.ondc.KeyUtil.DHKeyPair;
  * Class depicting the usage of the utility.
  */
 public class App {
+    private static Logger logger = Logger.getLogger("InfoLogger");
 
     /**
      * Main Function.
@@ -21,33 +24,33 @@ public class App {
         
         // Generate Key Pair for User 1.
         DHKeyPair keyPair1 = KeyUtil.generateKeyPair();
-        System.out.println("Key Pair 1 ==> "+ keyPair1.toString());
+        logger.log(Level.INFO, "Key Pair 1 ==> {0}", keyPair1);
         
         // Generate Key Pair for User 2.
         DHKeyPair keyPair2 = KeyUtil.generateKeyPair();
-        System.out.println("Key Pair 2 ==> "+ keyPair2.toString());
+        logger.log(Level.INFO, "Key Pair 2 ==> {0}", keyPair2);
 
         // Generate Shared Key with User 1's Private Key and User 2's Public Key.
         String sharedKey1 = KeyUtil.generateSharedKey(keyPair1.getPrivateKey(), keyPair2.getPublicKey());
-        System.out.println("SharedKey1 ==> "+ sharedKey1);
+        logger.log(Level.INFO, "SharedKey1 ==> {0}", sharedKey1);
         
         // Generate Shared Key with User 2's Private Key and User 1's Public Key.
         String sharedKey2 = KeyUtil.generateSharedKey(keyPair2.getPrivateKey(), keyPair1.getPublicKey());
-        System.out.println("SharedKey2 ==> "+ sharedKey2);
+        logger.log(Level.INFO, "SharedKey2 ==> {0}", sharedKey2);
 
         // Comparing the two Shared keys generated above.
-        System.out.println("sharedKey1 == sharedKey2 ==> " + sharedKey1.equals(sharedKey2));
+        logger.log(Level.INFO, "sharedKey1 == sharedKey2 ==> {0}", sharedKey1.equals(sharedKey2));
 
         // Initializing the raw text to be encrypted.
         String rawData = "Hello This is ONDC Test Data";
         
         // Encrypting the raw data.
         String encryptedData = EncryptionUtil.encryptData(sharedKey1, rawData);
-        System.out.println("Encrypted Data ===> " + encryptedData);
+        logger.log(Level.INFO, "Encrypted Data ===> {0}", encryptedData);
 
         // Decrypting the Encrypted data.
         String decryptedData =  EncryptionUtil.decryptData(sharedKey2, encryptedData);
-        System.out.println("Decrypted Data ===> " + decryptedData);
+        logger.log(Level.INFO, "Decrypted Data ===> {0}", decryptedData);
 
     }
 }
