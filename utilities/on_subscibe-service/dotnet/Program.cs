2c22bc77866860fb7c190d76122051801d3e2fcf
amitsinha07 <aamitsinha456@gmail.com>
2024-11-17 15:40:48 +0530

    feat: Did basic dotnet application setup with health check endpoint

---
+++ utilities/on_subscibe-service/dotnet/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Health check endpoint
+app.MapGet("/health", () => Results.Ok("Utility is running"))
+    .WithName("HealthCheck");
+
+app.Run();
+
