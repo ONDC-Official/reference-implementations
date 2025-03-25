915a3dcea3ebb6016642c31d81c3b57b61816ba4
Nishtha <nishthamonga07@gmail.com>
2025-03-25 17:53:09 +0530

    chore: readme and gitignore

--- utilities/on_subscibe-service/php/src/index.php
+++ utilities/on_subscibe-service/php/src/index.php
@@ -1,7 +1,6 @@
 <?php
 require 'vendor/autoload.php';
 use phpseclib3\Crypt\AES;
-use Ramsey\Uuid\Uuid;
 use Sop\CryptoTypes\Asymmetric\OneAsymmetricKey;
 use Sop\CryptoTypes\Asymmetric\PublicKeyInfo;
 
